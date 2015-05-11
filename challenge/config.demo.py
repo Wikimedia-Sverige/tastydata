37317b43309e80e2e6ba599bb79bef3365c37511
lokal-profil <lokal.profil@gmail.com>
2015-05-11 11:00:22 +0200

    example

---
+++ challenge/config.demo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+'''
+Configuration for the challengeStats bot. Don't commit!
+copy to config.py and fill in the details
+'''
+
+username = u''
+password = u''
+path = u''  # path from home directory to directory containing entities.json
