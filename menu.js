a57c9183d90d4ceeb0228585e979ada658400928
lokal-profil <lokal.profil@gmail.com>
2015-05-24 15:37:17 +0200

    extremely basic support of rtl

--- menu.js
+++ menu.js
@@ -2,6 +2,10 @@ $( document ).ready( function() {
 	$( '.uls-trigger' ).uls( {
 		onSelect : function( language ) {
 			$.qLabel.switchLanguage( language );
+			console.log( 'Selected: ' + language + ' (' +
+							$.uls.data.getDir(language) +
+						 ')' );
+			$( '.content' ).css( 'direction', $.uls.data.getDir( language ) );
 		},
 		quickList: ['en', 'de', 'es', 'fr', 'ru', 'ja', 'ko', 'sv']
 	} );
