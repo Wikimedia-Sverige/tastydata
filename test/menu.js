2d317ab9f04e0aec69133d97e6e459251f286cbd
lokal-profil <lokal.profil@gmail.com>
2015-06-03 01:09:58 +0300

    hacking

--- test/menu.js
+++ test/menu.js
@@ -26,7 +26,7 @@ $( document ).ready( function() {
 
 	//trigger lookup on popover event
 	//and close again if clicking anywhere except images inside the popup
-	$("popoverImg").on('click', function(){
+	$(".popoverImg").on('click', function(){
 	    return false;  // should prevent other kill triggers?
 	});
 	$("[data-toggle='popover']").on('click', function(){
@@ -167,34 +167,34 @@ function getURLParameter(param) {
 //** Call: uniquevar.playclip() to play sound
 
 var html5_audiotypes={ //define list of audio file extensions and their associated audio types. Add to it if your specified audio file isn't on this list:
-	"mp3": "audio/mpeg",
-	"mp4": "audio/mp4",
-	"ogg": "audio/ogg",
-	"wav": "audio/wav"
+    "mp3": "audio/mpeg",
+    "mp4": "audio/mp4",
+    "ogg": "audio/ogg",
+    "wav": "audio/wav"
 };
 
 function createsoundbite(sound){
     var html5audio = document.createElement('audio');
     if (html5audio.canPlayType){ //check support for HTML5 audio
-	for (var i=0; i<arguments.length; i++){
-		var sourceel=document.createElement('source');
-		sourceel.setAttribute('src', arguments[i]);
-		if (arguments[i].match(/\.(\w+)$/i)) {
-		    sourceel.setAttribute('type', html5_audiotypes[RegExp.$1]);
-		}
-		html5audio.appendChild(sourceel);
+	for (var i=0; i<arguments.length; i++) {
+	    var sourceel = document.createElement('source');
+	    sourceel.setAttribute('src', arguments[i]);
+	    if (arguments[i].match(/\.(\w+)$/i)) {
+		sourceel.setAttribute('type', html5_audiotypes[RegExp.$1]);
+	    }
+	    html5audio.appendChild(sourceel);
 	}
 	html5audio.load();
-	html5audio.playclip=function(){
+	html5audio.playclip = function(){
 	    html5audio.pause();
 	    html5audio.currentTime=0;
 	    html5audio.play();
 	};
 	return html5audio;
     }
-    else{
+    else {
 	return {
-	    playclip:function(){
+	    playclip: function(){
 		throw new Error("Your browser doesn't support HTML5 audio unfortunately");
 	    }
 	};
