9029cba2868d10d6cd7cfef8300f1387dcf0ab77
lokal-profil <lokal.profil@gmail.com>
2015-06-03 01:05:27 +0300

    hacking

--- test/menu.js
+++ test/menu.js
@@ -28,7 +28,7 @@ $( document ).ready( function() {
 	//and close again if clicking anywhere except images inside the popup
 	$("popoverImg").on('click', function(){
 	    return false;  // should prevent other kill triggers?
-	}
+	});
 	$("[data-toggle='popover']").on('click', function(){
 	    if ( popped ) {
 		$(this).popover('toggle');
@@ -171,27 +171,32 @@ var html5_audiotypes={ //define list of audio file extensions and their associat
 	"mp4": "audio/mp4",
 	"ogg": "audio/ogg",
 	"wav": "audio/wav"
-}
+};
 
 function createsoundbite(sound){
-	var html5audio=document.createElement('audio')
-	if (html5audio.canPlayType){ //check support for HTML5 audio
-		for (var i=0; i<arguments.length; i++){
-			var sourceel=document.createElement('source')
-			sourceel.setAttribute('src', arguments[i])
-			if (arguments[i].match(/\.(\w+)$/i))
-				sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
-			html5audio.appendChild(sourceel)
+    var html5audio = document.createElement('audio');
+    if (html5audio.canPlayType){ //check support for HTML5 audio
+	for (var i=0; i<arguments.length; i++){
+		var sourceel=document.createElement('source');
+		sourceel.setAttribute('src', arguments[i]);
+		if (arguments[i].match(/\.(\w+)$/i)) {
+		    sourceel.setAttribute('type', html5_audiotypes[RegExp.$1]);
 		}
-		html5audio.load()
-		html5audio.playclip=function(){
-			html5audio.pause()
-			html5audio.currentTime=0
-			html5audio.play()
-		}
-		return html5audio
+		html5audio.appendChild(sourceel);
 	}
-	else{
-		return {playclip:function(){throw new Error("Your browser doesn't support HTML5 audio unfortunately")}}
-	}
+	html5audio.load();
+	html5audio.playclip=function(){
+	    html5audio.pause();
+	    html5audio.currentTime=0;
+	    html5audio.play();
+	};
+	return html5audio;
+    }
+    else{
+	return {
+	    playclip:function(){
+		throw new Error("Your browser doesn't support HTML5 audio unfortunately");
+	    }
+	};
+    }
 }
