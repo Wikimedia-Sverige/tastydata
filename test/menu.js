4f9ff69bf86a63d87d45cf77d9fe9d00cb3550f2
lokal-profil <lokal.profil@gmail.com>
2015-06-02 19:33:09 +0300

    hacking

--- test/menu.js
+++ test/menu.js
@@ -89,7 +89,8 @@ function getClaims( entity, property, qualifier, qualifierValues ) {
 	    else if ( claim.qualifiers && qualifier in claim.qualifiers ) {
 		var qVal = 'Q' + claim.qualifiers[qualifier][0].datavalue.value['numeric-id'];
 		console.log(qVal + ' in ' + qualifierValues +' ?');
-		console.log($.inArray(qVal, qualifierValues));
+		console.log(typeof qualifierValues);
+		console.log($.inArray(qVal, qualifierValues)!==-1);
 		if ( qVal in qualifierValues ) {
 		    setClaim(property, claim.mainsnak.datavalue.value);
 		    return false;
